9c8891d008661d427a91b05e4eb07d7deab34705
Sasha Cilindz <sashacilindz@gmail.com>
2024-10-04 14:15:55 +0200

    init hw-47

--- src/components/SeletedPost/SelectedPost.tsx
+++ src/components/SeletedPost/SelectedPost.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState,useContext} from 'react';
 import './SelectedPost.css';
 import { useParams } from 'react-router-dom';
 import { ThemeContext } from '../../Providers/ThemeContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOnePost } from '../../slice/posts';
 
 type TData = {
     albumId: number,
@@ -19,16 +21,20 @@ function SelectedPost() {
 
     const {id} = useParams();
 
+    const dispatch = useDispatch();
+    const posts = useSelector((state:any) => state.posts);
+
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
-        .then(response => response.json())
-        .then(json => setPost(json))
-    });
+        // fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        // .then(response => response.json())
+        // .then(json => setPost(json))
+        dispatch<any>(fetchOnePost(id))
+    },[]);
 
     return ( <>
-        {post === null || post === undefined  ? <div></div> : <div className='container'>
-            <div className={`container-title-${color}`}>{post.title}</div>
-            <img src={post.url} alt='photo' className='container-photo'/>
+        {posts.selectedPost === null || posts.selectedPost === undefined  ? <div></div> : <div className='container'>
+            <div className={`container-title-${color}`}>{posts.selectedPost.title}</div>
+            <img src={posts.selectedPost.url} alt='photo' className='container-photo'/>
             <div className={`container-desription-${color}`}>{'This is discription '.repeat(20)}</div>
         </div>}
     </> );
--- src/slice/posts.tsx
+++ src/slice/posts.tsx
@@ -18,22 +18,39 @@ export const fetchPosts = createAsyncThunk(
 
     }
 )
+export const fetchOnePost = createAsyncThunk(
+    "posts/fetchOnePost",
+    async (id:string | undefined, {rejectWithValue}) => {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
+            if (!response.ok) {
+                throw new Error("Что-то пошло не так")
+            }
+            const data = await response.json();
+            
+            return data;
+        }
+
+        catch (error) {
+            return rejectWithValue((error as Error).message)
+        }
+
+    }
+)
 
 const postsSlice = createSlice({
     name: "posts",
     initialState:{
         posts:[],
+        selectedPost:{},
         favorites:[],
         error:null,
         status:null,
         activeTab:'all'
     },
     reducers:{
-
-
         setActiveTab(state:any, payload:any){
             state.activeTab = payload.payload;
-            console.log(state.activeTab)
         },
         addToFavorites(state:any,payload:any){
             const currentElem = state.posts.find((item:any) => item.id === payload.payload);
@@ -42,21 +59,32 @@ const postsSlice = createSlice({
         },
 
     },
-    extraReducers: (builder) => {
-        return builder.addCase(fetchPosts.pending, (state: any) => {
-            state.status = "loading";
-            state.error = null;
-        }),
-        builder.addCase(fetchPosts.fulfilled, (state: any, {payload}: {payload: any}) => {
-            state.status = "resolved";
-            state.error = null;
-            state.posts = payload;
-        }),
-        builder.addCase(fetchPosts.rejected, (state: any, {payload}: {payload: any}) => {
-            state.status = "rejected";
-            state.error = payload;
-            state.posts = [];
-        })
+    extraReducers: (builder:any) => {
+        return builder
+        .addCase(fetchPosts.pending, (state: any) => {
+                state.status = "loading";
+                state.error = null;
+            })
+            .addCase(fetchPosts.fulfilled, (state: any, {payload}: {payload: any}) => {
+                state.status = "resolved";
+                state.error = null;
+                state.posts = payload;
+            })
+            .addCase(fetchPosts.rejected, (state: any, {payload}: {payload: any}) => {
+                state.status = "rejected";
+                state.error = payload;
+                state.posts = [];
+            })
+
+            .addCase(fetchOnePost.pending, (state:any , {payload}:{payload:any}) => {
+                state.status = "loading";
+                state.error = null;
+            })
+            .addCase(fetchOnePost.fulfilled, (state: any, {payload}: {payload: any}) => {
+                state.status = "resolved";
+                state.error = null;
+                state.selectedPost = payload;
+            })
     }
 });
 
